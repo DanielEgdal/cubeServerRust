9836395e240c24433829c26315737d3462975108
Daniel V. Egdal <28397408+DanielEgdal@users.noreply.github.com>
2022-12-19 22:53:35 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,27 +16,35 @@ struct Scrmm{
     // link: ComponentLink<Self>,
     scram:String,
     sol: String,
-    to_dr_prune: HashMap<(u64,u64),Vec<u8>>
+    to_dr_prune: HashMap<(u64,u64),[u8;8]>,
+    to_finish_prune: HashMap<(u64,u64),[u8;8]>
 }
 
 impl Component for Scrmm{
     type Message =  Msg;
     type Properties = ();
     fn create(_ctx: &Context<Self>) -> Self{
-        Self {scram: "U R2 F B R B2 R U2 L B2 R U' D' R2 F R' L B2 U2 F2".to_string(), sol: String::from(""), to_dr_prune: HashMap::new()}
+        Self {scram: "U R2 F B R B2 R U2 L B2 R U' D' R2 F R' L B2 U2 F2".to_string(), 
+        sol: String::from(""), 
+        to_dr_prune: HashMap::new(),
+        to_finish_prune: HashMap::new()}
     }
 
     fn update(&mut self,_ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::InpScram(scram) => self.scram = scram.clone(),
+            Msg::InpScram(scram) => {
+                self.scram = scram.clone();
+                self.sol = "".to_string();
+            }
             Msg::Solve => {
-                
                 if self.to_dr_prune.is_empty(){
                     self.to_dr_prune = gen_eo_to_dr_prune();
-                    log!("prune generated");
+                    log!("dr prune generated");
+                    self.to_finish_prune = gen_finish_prune();
+                    log!("finish prune generated");
                 }
                 log!("solver started");
-                self.sol = solve_eo_from_scrm(self.scram.clone(),&self.to_dr_prune);
+                self.sol = solve_eo_from_scrm(self.scram.clone(),&self.to_dr_prune,&self.to_finish_prune);
             }
             
         }
@@ -56,7 +64,7 @@ impl Component for Scrmm{
                 <button onclick={
                     // println!("attempt1");
                     link.callback(|_| Msg::Solve)}>{ "Solve" }</button>
-                <p>{ format!("EO+DR solution: {}",self.sol)}</p>
+                <p>{ format!("Solution: {}",self.sol)}</p>
             </div>
 
         }
